734f79312ff44ef50649d0b9371d199dab9776cf
Ubuntu <ubuntu@ip-172-31-18-128.eu-west-1.compute.internal>
2018-04-05 12:27:46 +0000

    Removed Relation between Amenities and Listings

--- db/migrate/20180405102735_create_listings.rb
+++ db/migrate/20180405102735_create_listings.rb
@@ -2,7 +2,7 @@ class CreateListings < ActiveRecord::Migration[5.1]
   def change
     create_table :listings do |t|
       t.string :name
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user, foreign_key: true, index: true
 
       t.timestamps
     end
