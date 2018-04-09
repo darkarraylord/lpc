4dc1bbd058816f80963f048af819b31db0e5b956
Ubuntu <ubuntu@ip-172-31-18-128.eu-west-1.compute.internal>
2018-04-09 15:10:51 +0000

    prepare for datepicker

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,13 @@ class Booking < ApplicationRecord
   belongs_to :listing
   
   validates :status, inclusion: { in: ["pending", "denied", "approved"], allow_nil: false }
+  validates :introduction, length: {
+    minimum: 30,
+    maximum: 400,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "must have at least %{count} words",
+    too_long: "must have at most %{count} words"
+  }
   
   def approve!
     @booking.status = "approve"
